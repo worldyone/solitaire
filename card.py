2120b2958485349eff6b763b134d96538f8258d0
worldyone <amanohikari142857@gmail.com>
2024-12-31 12:32:02 +0900

    draggable pileを作成

--- card.py
+++ card.py
@@ -2,6 +2,7 @@ import flet as ft
 CARD_WIDTH = 70
 CARD_HEIGHT = 100
 DROP_PROXIMITY = 20
+CARD_OFFSET = 20
 
 
 class Card(ft.GestureDetector):
@@ -19,32 +20,52 @@ class Card(ft.GestureDetector):
         self.color = color
         self.content = ft.Container(
             bgcolor=self.color, width=CARD_WIDTH, height=CARD_HEIGHT)
+        self.draggable_pile = [self]
 
     def move_on_top(self):
-        """Moves draggable card to the top of the stack"""
-        self.solitaire.controls.remove(self)
-        self.solitaire.controls.append(self)
+        for card in self.draggable_pile:
+            self.solitaire.controls.remove(card)
+            self.solitaire.controls.append(card)
         self.solitaire.update()
 
     def bounce_back(self):
         """Returns card to its original position"""
-        self.top = self.slot.top
-        self.left = self.slot.left
-        self.update()
+        for card in self.draggable_pile:
+            self.top = card.slot.top + card.slot.pile.index(card) * CARD_OFFSET
+            self.left = self.slot.left
+        self.solitaire.update()
 
     def place(self, slot):
-        """Place card to the slot"""
-        self.top = slot.top
-        self.left = slot.left
+        for card in self.draggable_pile:
+            card.top = slot.top + len(slot.pile) * CARD_OFFSET
+            card.left = slot.left
+
+            # remove card from it's original slot, if exists
+            if self.slot is not None:
+                self.slot.pile.remove(self)
+
+            # change card's slot to a new slot
+            self.slot = slot
+
+            # add card to the new slot's pile
+            slot.pile.append(self)
 
     def start_drag(self, e: ft.DragStartEvent):
         self.move_on_top()
         self.update()
 
     def drag(self, e: ft.DragUpdateEvent):
-        self.top = max(0, self.top + e.delta_y)
-        self.left = max(0, self.left + e.delta_x)
-        self.update()
+        for card in self.draggable_pile:
+            card.top = (max(0, self.top + e.delta_y) +
+                        self.draggable_pile.index(card) * CARD_OFFSET)
+            card.left = max(0, self.left + e.delta_x)
+            self.solitaire.update()
+
+    def get_draggable_pile(self):
+        if self.slot is not None:
+            self.draggable_pile = self.slot.pile[self.slot.pile.index(self):]
+        else:
+            self.draggable_pile = [self]
 
     def drop(self, e: ft.DragEndEvent):
         for slot in self.solitaire.slots:
